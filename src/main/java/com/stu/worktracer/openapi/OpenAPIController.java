216e01025acf3b041dbdd2931c9edbc8ffaae66f
YGrylls <910357107@qq.com>
2020-04-15 22:42:19 +0800

    add TODOS

---
+++ src/main/java/com/stu/worktracer/openapi/OpenAPIController.java
@@ -0,0 +1,9 @@
+package com.stu.worktracer.openapi;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OpenAPIController {
+    //TODO
+}
---
+++ src/main/java/com/stu/worktracer/scheduled/Scheduler.java
@@ -0,0 +1,19 @@
+package com.stu.worktracer.scheduled;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Scheduler {
+
+
+    //TODO
+    public void monthlyRating() {
+
+    }
+
+
+    //TODO
+    public void dailyCollect() {
+
+    }
+}
